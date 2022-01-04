4784d2bcdfdf0220a7a40576783ae02d30651265
Andrew John Dougherty <adougher9@gmail.com>
2022-01-03 20:45:36 -0500

    Fixed some bugs, and reran frdcsa-build.sh.

--- examples/linux-remote-control-adapter/LinuxRemoteControlAdapterEnv.java
+++ examples/linux-remote-control-adapter/LinuxRemoteControlAdapterEnv.java
@@ -14,7 +14,7 @@ public class LinuxRemoteControlAdapterEnv extends Environment {
 
     @Override
     public void init(String[] args) {
-	Query.oneSolution("consult('jason_linux_remote_control_wrapper.pl')");
+    Query.oneSolution("consult('jason_linux_remote_control_wrapper.pl')");
         // initial percepts
         // addPercept(init0);
     }
@@ -28,38 +28,38 @@ public class LinuxRemoteControlAdapterEnv extends Environment {
 
         clearPercepts();
 
-	try {
-	    var string1 = "jpl_execute_action("+ag+","+act+",Result)";
-	    System.out.println("  " + string1);
-	    var solution1 =  Query.oneSolution(string1);
-	    var result1 = solution.get("Result");
-	    if (result1.isVariable()) {
-		System.out.println("\t" + ag + " has no return value");
-		// getEnvironmentInfraTier().getRuntimeServices().stopMAS();
-	    } else {
-		var string2 = "jpl_percepts("+ag+",Percepts,Result)";
-		System.out.println("  " + string2);
-		var solution2 =  Query.oneSolution(string2);
-		var percepts2 =  solution1.get("Percepts");
+    try {
+        var string1 = "jpl_execute_action("+ag+","+act+",Result)";
+        System.out.println("  " + string1);
+        var solution1 =  Query.oneSolution(string1);
+        var result1 = solution1.get("Result");
+        if (result1.isVariable()) {
+        System.out.println("\t" + ag + " has no return value");
+        // getEnvironmentInfraTier().getRuntimeServices().stopMAS();
+        } else {
+        var string2 = "jpl_percepts("+ag+",Percepts,Result)";
+        System.out.println("  " + string2);
+        var solution2 =  Query.oneSolution(string2);
+        var percepts2 =  solution2.get("Percepts");
 
-		// /System.out.println("\t" + ag + " currentEvents: " + currentEvents);
- 		Term[] arr = Util.listToTermArray(percepts2);
-		int i;
-		for (i = 0; i < arr.length; ++i) {
-		    Term oneTerm = arr[i];
-		    System.out.println("\t" + ag + " <<<" + oneTerm.toString() + ">>>");
-		    addPercept(Literal.parseLiteral(oneTerm.toString()));
-		}
-		var result2 = solution2.get("Result");
-		System.out.println("\t" + ag + " Result " + result.toString());
-	    } 
-	    System.out.println("");
-	    informAgsEnvironmentChanged();
-	    return true;
-	} catch (Exception e) {
+        // /System.out.println("\t" + ag + " currentEvents: " + currentEvents);
+        Term[] arr = Util.listToTermArray(percepts2);
+        int i;
+        for (i = 0; i < arr.length; ++i) {
+            Term oneTerm = arr[i];
+            System.out.println("\t" + ag + " <<<" + oneTerm.toString() + ">>>");
+            addPercept(Literal.parseLiteral(oneTerm.toString()));
+        }
+        var result2 = solution2.get("Result");
+        System.out.println("\t" + ag + " Result " + result2.toString());
+        } 
+        System.out.println("");
+        informAgsEnvironmentChanged();
+        return true;
+    } catch (Exception e) {
             System.out.println("error executing " + ag + " for " + act + ": " + e.toString());
-	    // throw e;
-	    return false;
-	}
+        // throw e;
+        return false;
+    }
     }
 }
