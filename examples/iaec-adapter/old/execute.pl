928ff7b11aac75078416f409deed09f5a41f9079
Andrew John Dougherty <adougher9@gmail.com>
2022-01-05 10:26:06 -0500

    Cleaned up extraneous files.

--- examples/iaec-adapter/old/execute.pl
+++
@@ -1,8 +0,0 @@
-% jpl_test_fac(+N, -F) :-
-%   F is the factorial of the positive integer N
-
-jpl_test_execute_action(Agent,Action,Result) :-
-	(   (	Action = lock) -> Result = locked ;
-	    (	(   Action = unlock) -> Result = unlocked ; true)).
-
-
--- examples/iaec-adapter/old/guardianagent.asl
+++
@@ -1,6 +0,0 @@
-// : not (dead(wife) & dead(husband)) <-
-
-+continue[_Annotations] <-
-    continue.
-
-// /var/lib/myfrdcsa/codebases/minor/strads-frdcsa/attempts/9/security/security_rules.pl
